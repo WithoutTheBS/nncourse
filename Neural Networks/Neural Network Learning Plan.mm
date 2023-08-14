dd7a01a77901a78e21d6581cbeff1096f07eac4a
Just a dude <106033984+WithoutTheBS@users.noreply.github.com>
2023-08-13 23:42:47 -0600

    Initial upload just to have an online backup.
    Not even close to halfway, boys.

---
+++ Neural Networks/Neural Network Learning Plan.mm
@@ -0,0 +1,328 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Neural Network Learning Plan" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1691982359807" NUMBERED="true">
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#2e3440" zoom="0.8">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_nord_template.mm" allow_compact_layout="true" followedTemplateLocation="template:/dark_nord_template.mm" followedMapLastTime="1690576022000" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" FORMAT_AS_HYPERLINK="true" BACKGROUND_COLOR="#81a1c1" NUMBERED="false">
+<font SIZE="10"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<node TEXT="Foundational Knowledge" POSITION="bottom_or_right" ID="ID_543446246" CREATED="1691975362570" MODIFIED="1691982359808" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<node TEXT="Math Basics" ID="ID_1051579469" CREATED="1691975991529" MODIFIED="1691982359808" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<node TEXT="Linear Algebra" ID="ID_314313116" CREATED="1691976301236" MODIFIED="1691982359808" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<node TEXT="Scalars, Vectors, Matrices, Tensors" ID="ID_1398575256" CREATED="1691976474564" MODIFIED="1691986626321" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<node TEXT="Scalars" ID="ID_730056156" CREATED="1691976691882" MODIFIED="1691988252781" LINK="../Neural%20Networks/html/1111/scalars.html">
+<attribute_layout NAME_WIDTH="66.75 pt" VALUE_WIDTH="98.25 pt"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="font-size: 9pt;">Single numbers.</span>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Vectors" ID="ID_1224791423" CREATED="1691976699745" MODIFIED="1691986685802" LINK="html/1111/vectors.html" NUMBERED="false">
+<attribute_layout NAME_WIDTH="66.75 pt" VALUE_WIDTH="196.49999 pt"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="xml/auto">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="font-size: 9pt;">Arrays of numbers with one dimension.</span>
+    </p>
+  </body>
+</html></richcontent>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+<node TEXT="Matrices" ID="ID_1370989890" CREATED="1691976717501" MODIFIED="1691986757142" LINK="../Neural%20Networks/html/1111/matrices.html" NUMBERED="false">
+<attribute_layout NAME_WIDTH="66.75 pt" VALUE_WIDTH="186.74999 pt"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="xml/auto">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="font-size: 9pt;">Two-dimensional arrays of numbers.</span>
+    </p>
+  </body>
+</html></richcontent>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+<node TEXT="Tensors" ID="ID_959565854" CREATED="1691976722922" MODIFIED="1691986778626" LINK="../Neural%20Networks/html/1111/tensors.html" NUMBERED="false">
+<attribute_layout NAME_WIDTH="66.75 pt" VALUE_WIDTH="209.24999 pt"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="xml/auto">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="font-size: 9pt;">Multi-dimensional arrays (beyond just 2D).</span>
+    </p>
+  </body>
+</html></richcontent>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+</node>
+<node TEXT="Basic Matrix Operations" ID="ID_1678637567" CREATED="1691976517460" MODIFIED="1691982359823" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<node TEXT="Addition and Subtraction" ID="ID_31738536" CREATED="1691988178874" MODIFIED="1691988279018"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Element-wise operations.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Scalar Multiplication" ID="ID_409558921" CREATED="1691988181068" MODIFIED="1691988306895"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Multiplying a matrix by a scalar.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Matrix Multiplication (Dot Product)" ID="ID_356341005" CREATED="1691988181388" MODIFIED="1691988341223"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Matrix product, a fundamental operation in neural networks.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Matrix Properties and Special Types of Matrices" ID="ID_491445025" CREATED="1691976531402" MODIFIED="1691982359823" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<node TEXT="Identity Matrix" ID="ID_1593230878" CREATED="1691988353549" MODIFIED="1691988550002"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The matrix equivalent of the number one.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Transpose" ID="ID_1049230223" CREATED="1691988357697" MODIFIED="1691988501661"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Reflecting a matrix over its main diagonal.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Inverse" ID="ID_719996481" CREATED="1691988358065" MODIFIED="1691988473825"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Only square matrices have inverses, and they are used to solve systems of linear equations.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Orthogonal Matrices" ID="ID_970964840" CREATED="1691988358533" MODIFIED="1691988433306"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Matrices with columns and rows that are unit vectors and orthogonal to each other.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Determinants and Rank" ID="ID_736849057" CREATED="1691976551494" MODIFIED="1691982359823" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+<node TEXT="Eigenvalues and Eigenvectors" ID="ID_1670951336" CREATED="1691976558205" MODIFIED="1691982359823" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+<node TEXT="Singular Value Decomposition" ID="ID_188612015" CREATED="1691976559319" MODIFIED="1691982359824" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+<node TEXT="Linear Transformations" ID="ID_1878198864" CREATED="1691976559509" MODIFIED="1691982359824" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+<node TEXT="Inner Product, Length and Orthogonality" ID="ID_1695718452" CREATED="1691976559912" MODIFIED="1691982359824" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+<node TEXT="Systems of Linear Equations" ID="ID_1179103939" CREATED="1691976564017" MODIFIED="1691982359824" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+</node>
+<node TEXT="Calculus" ID="ID_494019551" CREATED="1691976439309" MODIFIED="1691982359824" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+<node TEXT="Statistics" ID="ID_943766805" CREATED="1691976460784" MODIFIED="1691982359824" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+</node>
+<node TEXT="Neural Network Theory" ID="ID_1845949062" CREATED="1691976042718" MODIFIED="1691982359824" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+</node>
+<node TEXT="Programming Language &amp; Libraries" POSITION="bottom_or_right" ID="ID_809694601" CREATED="1691975362570" MODIFIED="1691982359824" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<node TEXT="Rust" ID="ID_95572768" CREATED="1691976070885" MODIFIED="1691979865308" NUMBERED="true"/>
+<node TEXT="ASM" ID="ID_719698809" CREATED="1691976091157" MODIFIED="1691979869333" NUMBERED="true"/>
+</node>
+<node TEXT="Hands-On Projects" POSITION="bottom_or_right" ID="ID_1029363176" CREATED="1691975362570" MODIFIED="1691982359824" TEXT_SHORTENED="true" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+<node TEXT="Online Resources" POSITION="bottom_or_right" ID="ID_1210031803" CREATED="1691975362570" MODIFIED="1691982359825" TEXT_SHORTENED="true" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<node TEXT="Courses" ID="ID_1302287070" CREATED="1691976127893" MODIFIED="1691979898514" NUMBERED="true"/>
+<node TEXT="Tutorials" ID="ID_1330463915" CREATED="1691976143060" MODIFIED="1691979905680" NUMBERED="true"/>
+<node TEXT="Books" ID="ID_1730984595" CREATED="1691976143700" MODIFIED="1691979908581" NUMBERED="true"/>
+</node>
+<node TEXT="Iterations and Experiments" POSITION="bottom_or_right" ID="ID_608533590" CREATED="1691975362570" MODIFIED="1691982359825" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<node TEXT="Tools" ID="ID_1007966288" CREATED="1691976187303" MODIFIED="1691979912312" NUMBERED="true"/>
+</node>
+<node TEXT="Communities and Collaborations" POSITION="bottom_or_right" ID="ID_1822145793" CREATED="1691975362570" MODIFIED="1691982359825" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<node TEXT="Forums" ID="ID_1635492212" CREATED="1691976206369" MODIFIED="1691979914402" NUMBERED="true"/>
+<node TEXT="GitHub" ID="ID_1288487385" CREATED="1691976226079" MODIFIED="1691979916653" NUMBERED="true"/>
+</node>
+<node TEXT="Reviews and Updates" POSITION="bottom_or_right" ID="ID_483361655" CREATED="1691975362570" MODIFIED="1691982359825" NUMBERED="true">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</node>
+</node>
+</map>
